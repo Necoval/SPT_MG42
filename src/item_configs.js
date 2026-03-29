a7de9aacffaa8aff8238ce608a19d2964b6053f6
Necoval <32438933+Necoval@users.noreply.github.com>
2026-03-29 18:57:28 +0200

    Add files via upload

---
+++ src/item_configs.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const configDirectories = [
+    path.resolve(__dirname, '../config/cards'),
+    path.resolve(__dirname, '../config/packs')
+];
+const customItemConfigs = [];
+configDirectories.forEach(directory => {
+    fs.readdirSync(directory).forEach(file => {
+        if (file.endsWith('.json')) {
+            const config = require(path.join(directory, file));
+            customItemConfigs.push(config);
+        }
+    });
+});
+module.exports = { customItemConfigs };
+//# sourceMappingURL=item_configs.js.map
